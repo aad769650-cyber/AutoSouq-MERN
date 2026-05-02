5f0f2d07c730865a2a7bf039e20cf6fdf788e99b
Mahar Abdul Qudoos <aad769650@gmail.com>
2026-05-02 22:40:20 +0500

    make changes for production url

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios"
 import { useNavigate } from "react-router-dom";
 const api=axios.create({
-    baseURL:"http://localhost:8000"
+    baseURL:"https://autosouq-mern-backend.onrender.com",
+    withCredentials:true
 })
 
 export const SellCar=async(formData)=>{
